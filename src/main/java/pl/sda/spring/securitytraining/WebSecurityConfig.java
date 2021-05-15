fe23ff2e61466c456eb59eda5aa2d868db453023
Bartosz Kończyk <bartkon@gmail.com>
2021-05-15 13:19:44 +0200

    h2-console enabled

--- src/main/java/pl/sda/spring/securitytraining/WebSecurityConfig.java
+++ src/main/java/pl/sda/spring/securitytraining/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package pl.sda.spring.securitytraining;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,11 +12,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.headers().frameOptions().disable();
+
         http.httpBasic()
             .and()
             .authorizeRequests()
             .antMatchers("/hello").permitAll()
             .antMatchers("/admin").hasRole("ADMIN")
+            .antMatchers("/h2-console/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin().permitAll()
